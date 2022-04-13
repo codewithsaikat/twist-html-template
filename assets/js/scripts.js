9ea95e493fa19c3064e5107730c2a94ff177e96e
saikat007hasan <saikathasan441@gmail.com>
2022-04-13 14:30:57 +0600

    footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,12 +4,40 @@ $(document).ready(function () {
        dots: true,
     });
 
-   $('.team-list').slick({
-     autoplay: false,
-     infinite: true,
-     slidesToShow: 3,
-     slidesToScroll: 3,
-     dots: true,
+  $('.team-list').slick({
+    autoplay: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+    
+  
+     
        
    });
 
@@ -18,7 +46,32 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    dots: true,
+     dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
 
    })
    $('.logo-carousel').slick({
@@ -26,7 +79,32 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 5,
-    dots: true,
+     dots: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
     
 
    })
